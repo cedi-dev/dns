b3f0ceb63bf43febe8696cc0f69c68731a4e64f8
Cedric Kienzler <github@cedric-kienzler.de>
2022-06-23 14:36:16 +0200

    fix k8s

--- blog.tf
+++ blog.tf
@@ -1,26 +1,17 @@
-resource "cloudflare_record" "k8s_ingress_v4" {
+resource "cloudflare_record" "blog" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
-  value   = "167.233.13.49"
-  type    = "A"
-  ttl     = 1
-  proxied = false
-}
-
-resource "cloudflare_record" "k8s_ingress_v6" {
-  zone_id = var.cloudflare_zone_id
-  name    = "www"
-  value   = "2a01:4f8:1c1d:7e4::1"
-  type    = "AAAA"
-  ttl     = 1
-  proxied = false
-}
-
-resource "cloudflare_record" "k8s_dash" {
-  zone_id = var.cloudflare_zone_id
-  name    = "dash"
-  value   = "cedi-dev-github-io.pages.dev"
+  value   = "cedi-github-io.pages.dev"
   type    = "CNAME"
   ttl     = 1
-  proxied = false
+  proxied = true
+}
+
+resource "cloudflare_record" "blog_www" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www"
+  value   = "cedi-github-io.pages.dev"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
 }
